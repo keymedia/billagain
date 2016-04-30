b10df2a9788bc4be05d769ef2637cd4d55551c26
Mark Duppa-Whyte <support@keymedia.co.za>
2016-04-30 10:20:09 +0200

    completed customer section of API beta untested

--- src/BillAgain/Customers/CustomerNotes.php
+++ src/BillAgain/Customers/CustomerNotes.php
@@ -2,6 +2,131 @@
 
 namespace BillAgain;
 
-class CustomerNotes {
-    
+use BillAgain\Curl;
+
+/**
+ * Customers.
+ * 
+ * Main class for dealing with Customers
+ * 
+ * @link http://docs.billagain.com/developer/customers
+ * @version 1.0.1
+ * @license GPL
+ * @author Mark Duppa-Whyte <mark@keymedia.co.za>
+ * 
+ */
+class CustomerNotes extends Curl {
+
+    /**
+     * @var integer $customerId
+     */
+    protected $customerId;
+
+    /**
+     * @var integer $customerNoteId
+     */
+    protected $customerNoteId;
+
+    /**
+     * Returns a list off all customer notes linked to this customerID which is linked to the logged in account
+     * 
+     * @param integer $customerId
+     */
+    public function listByCustomer($customerId) {
+        if (gettype($customerId) == 'integer') {
+            $this->customerId = $customerId;
+            $this->type = 'GET';
+            $this->url = '/customers/' . $this->customerId . '/customernotes';
+            $this->initCurl();
+        } else {
+            $this->error = 'Customer ID is not an integer value';
+        }
+        return $this;
+    }
+
+    /**
+     * Returns a list off all customer notes linked to the logged in account
+     * 
+     */
+    public function listCustomerNotes() {
+        $this->type = 'GET';
+        $this->url = '/customernotes';
+        $this->initCurl();
+        return $this;
+    }
+
+    /**
+     * Returns a specific customer note
+     * 
+     * @param integer $customerNoteId
+     */
+    public function getCustomerNotes($customerNoteId) {
+        if (gettype($customerNoteId) == 'integer') {
+            $this->customerNoteId = $customerNoteId;
+            $this->type = 'GET';
+            $this->url = '/customernotes/' . $this->customerNoteId;
+            $this->initCurl();
+        } else {
+            $this->error = 'Customer Note ID is not an integer value';
+        }
+        return $this;
+    }
+
+    /**
+     * Adds a customer note to the customer which is linked to your account
+     * Please check API documentation for required fields
+     * 
+     * @param array $data
+     */
+    public function addCustomerNote($data = array()) {
+        $this->setData($data);
+        if ($this->error == '') {
+            $this->type = 'POST';
+            $this->url = '/customernotes';
+            $this->initCurl();
+        }
+        return $this;
+    }
+
+    /**
+     * Edits a customer's note linked to your account
+     * Please check API documentation for required fields
+     * 
+     * @param integer $customerNoteId
+     * @param array $data
+     * @return \BillAgain\Customers
+     */
+    public function editCustomerNote($customerNoteId, $data = array()) {
+        if (gettype($customerNoteId) == 'integer') {
+            $this->customerNoteId = $customerNoteId;
+            $this->setData($data);
+            if ($this->error == '') {
+                $this->type = 'PUT';
+                $this->url = '/customernotes/' . $this->customerNoteId;
+                $this->initCurl();
+            }
+        } else {
+            $this->error = 'Customer Note ID is not an integer value';
+        }
+        return $this;
+    }
+
+    /**
+     * Delete a customer note from your account
+     * 
+     * @param integer $customerNoteId
+     * @return \BillAgain\Customers
+     */
+    public function deleteCustomerNote($customerNoteId) {
+        if (gettype($customerNoteId) == 'integer') {
+            $this->customerNoteId = $customerNoteId;
+            $this->type = 'DELETE';
+            $this->url = '/customernotes/' . $this->customerNoteId;
+            $this->initCurl();
+        } else {
+            $this->error = 'Customer Note ID is not an integer value';
+        }
+        return $this;
+    }
+
 }
--- src/BillAgain/Customers/Customers.php
+++ src/BillAgain/Customers/Customers.php
@@ -18,14 +18,13 @@ use BillAgain\Curl;
 class Customers extends Curl {
 
     /**
-     * The unique Id set for individual customer
-     * @var integer customer_id
+     * @var integer $customerId
      */
-    protected $customer_id;
+    protected $customerId;
 
     /**
-     * Lists all the customers
-     * Calls the initCurl method without extra parameters
+     * Returns a list off all the Customers linked to the logged in account, subject to paging as defined in pagination section
+     * 
      */
     public function listCustomers() {
         $this->initCurl();
@@ -33,26 +32,26 @@ class Customers extends Curl {
     }
 
     /**
-     * Get the customer details of a given Id
-     * @param integer $customer_id
+     * Returns a specific customer
+     * 
+     * @param integer $customerId
      */
-    public function getCustomer($customer_id) {
-        if (gettype($customer_id) == 'integer') {
-            $this->customer_id = $customer_id;
+    public function getCustomer($customerId) {
+        if (gettype($customerId) == 'integer') {
+            $this->customerId = $customerId;
             $this->type = 'GET';
-            $this->url = '/customers/' . $this->customer_id;
+            $this->url = '/customers/' . $this->customerId;
             $this->initCurl();
         } else {
-            $this->error = 'Customer Id is not an integer value';
+            $this->error = 'Customer ID is not an integer value';
         }
         return $this;
     }
 
     /**
-     * This adds a customer via POST method, data is inserted as an array
-     * 
+     * Adds a customer to your account
      * Please check API documentation for required fields
-     * 
+     *  
      * @param array $data
      */
     public function addCustomer($data = array()) {
@@ -65,75 +64,78 @@ class Customers extends Curl {
     }
 
     /**
-     * Edit Customer, passing through data in an array format
+     * Edits a specific customer details
+     * Please check API documentation for required fields
      * 
-     * @param integer $customer_id
+     * @param integer $customerId
      * @param array $data
      * @return \BillAgain\Customers
      */
-    public function editCustomer($customer_id, $data = array()) {
-        if (gettype($customer_id) == 'integer') {
-            $this->customer_id = $customer_id;
+    public function editCustomer($customerId, $data = array()) {
+        if (gettype($customerId) == 'integer') {
+            $this->customerId = $customerId;
             $this->setData($data);
             if ($this->error == '') {
                 $this->type = 'PUT';
-                $this->url = '/customers/' . $this->customer_id;
+                $this->url = '/customers/' . $this->customerId;
                 $this->initCurl();
             }
+        } else {
+            $this->error = 'Customer ID is not an integer value';
         }
         return $this;
     }
 
     /**
-     * Delete Customer
+     * Delete a customer from your account
      * 
-     * @param integer $customer_id
+     * @param integer $customerId
      * @return \BillAgain\Customers
      */
-    public function deleteCustomer($customer_id) {
-        if (gettype($customer_id) == 'integer') {
-            $this->customer_id = $customer_id;
+    public function deleteCustomer($customerId) {
+        if (gettype($customerId) == 'integer') {
+            $this->customerId = $customerId;
             $this->type = 'DELETE';
-            $this->url = '/customers/' . $this->customer_id;
+            $this->url = '/customers/' . $this->customerId;
             $this->initCurl();
         } else {
-            $this->error = 'Customer Id is not an integer value';
+            $this->error = 'Customer ID is not an integer value';
         }
         return $this;
     }
-    
+
     /**
-     * Archive Customer
+     * Archive a customer from your account
      * 
-     * @param integer $customer_id
+     * @param integer $customerId
      * @return \BillAgain\Customers
      */
-    public function archiveCustomer($customer_id) {
-        if (gettype($customer_id) == 'integer') {
-            $this->customer_id = $customer_id;
+    public function archiveCustomer($customerId) {
+        if (gettype($customerId) == 'integer') {
+            $this->customerId = $customerId;
             $this->type = 'PUT';
-            $this->url = '/customers/' . $this->customer_id . '/archive';
+            $this->url = '/customers/' . $this->customerId . '/archive';
             $this->initCurl();
         } else {
-            $this->error = 'Customer Id is not an integer value';
+            $this->error = 'Customer ID is not an integer value';
         }
         return $this;
     }
-    
+
     /**
-     * Reopen Customer
+     * Reopen a customer from your account
      * 
-     * @param integer $customer_id
+     * @param integer $customerId
      * @return \BillAgain\Customers
      */
-    public function reopenCustomer($customer_id) {
-        if (gettype($customer_id) == 'integer') {
-            $this->customer_id = $customer_id;
+    public function reopenCustomer($customerId) {
+        if (gettype($customerId) == 'integer') {
+            $this->customerId = $customerId;
             $this->type = 'PUT';
-            $this->url = '/customers/' . $this->customer_id . '/reopen';
+            $this->url = '/customers/' . $this->customerId . '/reopen';
             $this->initCurl();
         } else {
-            $this->error = 'Customer Id is not an integer value';
+            $this->error = 'Customer ID is not an integer value';
         }
         return $this;
     }
